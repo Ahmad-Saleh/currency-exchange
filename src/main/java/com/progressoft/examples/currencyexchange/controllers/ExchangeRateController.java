50a00c20f1d7355eeb84ec51dceb4559f913c3d5
Ahmad Saleh <ahmad.saleh@progressoft.com>
2024-04-18 09:55:51 +0300

    chore: initial commit

--- src/main/java/com/progressoft/examples/currencyexchange/controllers/ExchangeRateController.java
+++ src/main/java/com/progressoft/examples/currencyexchange/controllers/ExchangeRateController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-
 @RestController
 @RequiredArgsConstructor
 public class ExchangeRateController {
@@ -16,7 +14,7 @@ public class ExchangeRateController {
     private final ExchangeRateService exchangeRateService;
 
     @PostMapping("/api/v1/calculate-exchange-rate")
-    public String getExchangeRate(@RequestBody ExchangeRequest exchangeRequest){
+    public String calculateExchangeRate(@RequestBody ExchangeRequest exchangeRequest){
         return exchangeRateService.getExchangeRate(exchangeRequest).toString();
     }
 }
